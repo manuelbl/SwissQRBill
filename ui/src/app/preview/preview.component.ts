50def59247982d6ec6fc2cc66d3e4b72848c2739
Manuel Bleichenbacher <manuel.bleichenbacher@gmail.com>
2018-12-14 21:05:37 +0100

    Set size of image to prevent resizing during animation

--- ui/src/app/preview/preview.component.ts
+++ ui/src/app/preview/preview.component.ts
@@ -6,6 +6,7 @@
 //
 import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { QrBill } from '../qrbill-api/qrbill';
 
 @Component({
   selector: 'qrbill-preview',
@@ -14,9 +15,28 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class PreviewComponent implements OnInit {
   public billID: string;
+  public imageWidth: string;
+  public imageHeight: string;
 
   constructor(@Inject(MAT_DIALOG_DATA) private data: any) {
     this.billID = data.billID;
+
+    const bill: QrBill = data.validatedBill;
+    const outputSize = bill.format.outputSize;
+
+    // Set the image size to prevent the dialog from resizing
+    // in the middle of the opening animation.
+    // The values are calculated as: dim_in_mm / 25.4 mm/in * 96 px/in
+    if (outputSize === 'qr-bill-only') {
+      this.imageWidth = '793.700';
+      this.imageHeight = '396.850';
+    } else if (outputSize === 'a4-portrait-sheet') {
+      this.imageWidth = '793.700';
+      this.imageHeight = '1122.519';
+    } else if (outputSize === 'qr-code-only') {
+      this.imageWidth = '173.858';
+      this.imageHeight = '173.858';
+    }
   }
 
   ngOnInit() {}
